312defea87e8f1d877ebaaf31069e21a284114f4
Jordy Marquez <jordymarquez@MacBook-Pro.local>
2023-05-04 21:21:54 -0700

    added github icon in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import Icon from './github.png'
 
 // A single Footer component that appears on multiple pages
 function Footer() {
     return (
-        <div>
-            
+        <div className="content is-vcen">
+            <a href="https://github.com/JordyMarquez">
+
+                <img src={Icon} alt="github" style={{ height: 50, width: 50 }}>
+
+                </img>
+
+            </a>
+
             <p>©️ Orchidia Coding 2023</p>
         </div>
     )
 }
 
-export default Footer; 
+export default Footer;
 
 // WHEN I view the footer
 // THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -47,6 +47,7 @@ export default function Contact() {
                     </span>
 
                 </div>
+                <button>Submit</button>
             </form>
         </div>
     )
